7a9fac33d1724cd8b6762d7fccb8e3f2c732dca4
maku85 <makuma85@gmail.com>
2022-01-30 11:16:14 +0100

    test: improve tests

--- test/person.test.js
+++ test/person.test.js
@@ -7,65 +7,138 @@ describe('The person generator', () => {
     const res = dummy.person.firstName();
     console.log({ res });
     expect(res).to.be.not.undefined;
+    expect(res).to.be.not.null;
   });
 
   it('generates random male firstName', () => {
     const res = dummy.person.firstName('male');
     console.log({ res });
     expect(res).to.be.not.undefined;
+    expect(res).to.be.not.null;
   });
 
   it('generates random female firstName', () => {
     const res = dummy.person.firstName('female');
     console.log({ res });
     expect(res).to.be.not.undefined;
+    expect(res).to.be.not.null;
   });
 
   it('generates random lastName', () => {
     const res = dummy.person.lastName();
     console.log({ res });
     expect(res).to.be.not.undefined;
+    expect(res).to.be.not.null;
   });
 
   it('gives random gender', () => {
     const res = dummy.person.gender();
     console.log({ res });
     expect(res).to.be.not.undefined;
+    expect(res).to.be.not.null;
   });
 
   it('gives random binary gender', () => {
     const res = dummy.person.gender(true);
     console.log({ res });
     expect(res).to.be.not.undefined;
+    expect(res).to.be.not.null;
   });
 
   it('gives random non binary gender', () => {
     const res = dummy.person.gender(false);
     console.log({ res });
     expect(res).to.be.not.undefined;
+    expect(res).to.be.not.null;
   });
 
   it('generates random title', () => {
     const res = dummy.person.title();
     console.log({ res });
     expect(res).to.be.not.undefined;
+    expect(res).to.be.not.null;
   });
 
   it('generates random male title', () => {
     const res = dummy.person.title('male');
     console.log({ res });
     expect(res).to.be.not.undefined;
+    expect(res).to.be.not.null;
   });
 
   it('generates random female title', () => {
     const res = dummy.person.title('female');
     console.log({ res });
     expect(res).to.be.not.undefined;
+    expect(res).to.be.not.null;
   });
 
   it('generates random jobTitle', () => {
     const res = dummy.person.jobTitle();
     console.log({ res });
     expect(res).to.be.not.undefined;
+    expect(res).to.be.not.null;
+  });
+
+  describe('Localized generator', () => {
+    beforeEach(() => {
+      dummy.locale = 'it';
+    });
+
+    it('generates random firstName', () => {
+      const res = dummy.person.firstName();
+      console.log({ res });
+      expect(res).to.be.not.undefined;
+      expect(res).to.be.not.null;
+    });
+
+    it('generates random male firstName', () => {
+      const res = dummy.person.firstName('male');
+      console.log({ res });
+      expect(res).to.be.not.undefined;
+      expect(res).to.be.not.null;
+    });
+
+    it('generates random female firstName', () => {
+      const res = dummy.person.firstName('female');
+      console.log({ res });
+      expect(res).to.be.not.undefined;
+      expect(res).to.be.not.null;
+    });
+
+    it('generates random lastName', () => {
+      const res = dummy.person.lastName();
+      console.log({ res });
+      expect(res).to.be.not.undefined;
+      expect(res).to.be.not.null;
+    });
+
+    it('gives random binary gender', () => {
+      const res = dummy.person.gender(true);
+      console.log({ res });
+      expect(res).to.be.not.undefined;
+      expect(res).to.be.not.null;
+    });
+
+    it('generates random title', () => {
+      const res = dummy.person.title();
+      console.log({ res });
+      expect(res).to.be.not.undefined;
+      expect(res).to.be.not.null;
+    });
+
+    it('generates random male title', () => {
+      const res = dummy.person.title('male');
+      console.log({ res });
+      expect(res).to.be.not.undefined;
+      expect(res).to.be.not.null;
+    });
+
+    it('generates random female title', () => {
+      const res = dummy.person.title('female');
+      console.log({ res });
+      expect(res).to.be.not.undefined;
+      expect(res).to.be.not.null;
+    });
   });
 });
